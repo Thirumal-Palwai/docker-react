abf56340d6f8198c5e66ce99c4a623c02da38281
Thirumal-Palwai <thirumal.palwai@gmail.com>
2020-08-13 18:58:40 +0530

    Text change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          P Thirumal Reddy.
+          P Thirumal.
         </p>
         <a
           className="App-link"
